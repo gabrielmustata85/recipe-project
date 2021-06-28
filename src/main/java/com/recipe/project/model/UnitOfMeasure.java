7ec6d23c9085ca45c012f4cd242408a601e4a8ef
gabrielmustata85 <gabrielmstt@gmail.com>
2021-06-28 19:32:30 +0300

    lombok added

--- src/main/java/com/recipe/project/model/UnitOfMeasure.java
+++ src/main/java/com/recipe/project/model/UnitOfMeasure.java
@@ -1,31 +1,19 @@
 package com.recipe.project.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 public class UnitOfMeasure {
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long Id;
-
     private String description;
 
 
 
-    public Long getId() {
-        return Id;
-    }
 
-    public void setId(Long id) {
-        Id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
