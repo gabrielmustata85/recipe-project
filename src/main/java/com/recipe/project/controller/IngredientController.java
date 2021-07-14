4e68f6452b11100f738caf967b270dca370e45ac
gabrielmustata85 <gabrielmstt@gmail.com>
2021-07-14 18:15:58 +0300

    add-delete ingredient

--- src/main/java/com/recipe/project/controller/IngredientController.java
+++ src/main/java/com/recipe/project/controller/IngredientController.java
@@ -2,6 +2,8 @@ package com.recipe.project.controller;
 
 
 import com.recipe.project.commands.IngredientCommand;
+import com.recipe.project.commands.RecipeCommand;
+import com.recipe.project.commands.UnitOfMeasureCommand;
 import com.recipe.project.service.IngredientService;
 import com.recipe.project.service.RecipeService;
 import com.recipe.project.service.UnitOfMeasureService;
@@ -44,6 +46,28 @@ public class IngredientController {
     }
 
     @GetMapping
+    @RequestMapping("recipe/{recipeId}/ingredient/new")
+    public String newIngredient(@PathVariable String recipeId, Model model){
+
+        //make sure we have a good id value
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+        //todo raise exception if null
+
+        //need to return back parent id for hidden form property
+        IngredientCommand ingredientCommand = new IngredientCommand();
+        ingredientCommand.setRecipeId(Long.valueOf(recipeId));
+        model.addAttribute("ingredient", ingredientCommand);
+
+        //init uom
+        ingredientCommand.setUom(new UnitOfMeasureCommand());
+
+        model.addAttribute("uomList",  unitOfMeasureService.listAllUoms());
+
+        return "recipe/ingredient/ingredientform";
+    }
+
+
+    @GetMapping
     @RequestMapping("recipe/{recipeId}/ingredient/{id}/update")
     public String updateRecipeIngredient(@PathVariable String recipeId,
                                          @PathVariable String id, Model model) {
@@ -61,5 +85,19 @@ public class IngredientController {
         log.debug("saved ingredient id:" + savedCommand.getId());
 
         return "redirect:/recipe/" + savedCommand.getRecipeId() + "/ingredient/" + savedCommand.getId() + "/show";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{recipeId}/ingredient/{id}/delete")
+    public String deleteIngredient(@PathVariable String recipeId,
+                                   @PathVariable String id){
+
+        log.debug("deleting ingredient id:" +id);
+        ingredientService.deleteById(Long.valueOf(recipeId), Long.valueOf(id));
+
+        return "redirect:/recipe/" + recipeId + "/ingredients";
+
+
+
     }
 }
--- src/test/java/com/recipe/project/model/CategoryTest.java
+++ src/test/java/com/recipe/project/model/CategoryTest.java
@@ -2,7 +2,6 @@ package com.recipe.project.model;
 
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class CategoryTest {
